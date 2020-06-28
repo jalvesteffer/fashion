8c3458f2ab74f3fe57e93373e19e4d8a588371f2
Matt <mgmcintyre777@gmail.com>
2020-06-28 15:35:42 -0400

    quick fix to ShopService to show example usage of ServerService

--- src/app/common/services/shop.service.ts
+++ src/app/common/services/shop.service.ts
@@ -1,27 +1,26 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { ServerService } from './server.service';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
+import { ServerService } from "./server.service";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root",
 })
 export class ShopService {
+  constructor(private server: ServerService) {}
 
-    constructor(private http: ServerService) { }
+  getAll(url) {
+    return this.server.request("GET", url);
+  }
 
-    getAll(url) {
-        return this.http.request('GET', url);
-    }
+  deleteObj(url) {
+    return this.server.request("DELETE", url);
+  }
 
-    deleteObj(url) {
-        return this.http.delete(url);
-    }
+  updateObj(url, obj) {
+    return this.server.request("PUT", url, obj);
+  }
 
-    //   updateObj(url, obj) {
-    //     return this.http.put(url, obj);
-    //   }
-
-    postObj(url, obj) {
-        return this.http.request('POST', url, obj);
-    }
+  postObj(url, obj) {
+    return this.server.request("POST", url, obj);
+  }
 }
