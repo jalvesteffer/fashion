0ee578efc10f28405a633eebbbf997ecd2544a6d
jalvesteffer <jason.alvesteffer@smoothstack.com>
2020-07-02 23:19:22 -0400

    Pagination workaround

--- src/app/common/services/pager.service.ts
+++ src/app/common/services/pager.service.ts
@@ -6,22 +6,23 @@ export class PagerService {
   getPager(totalItems: number, currentPage: number = 1, pageSize: number) {
     // calculate total pages
     const totalPages = Math.ceil(totalItems / pageSize);
+    console.log("this is" + totalPages)
     let startPage: number, endPage: number;
-    if (totalPages <= 10) {
-      // less than 10 total pages so show all
+    if (totalPages <= pageSize) {
+      // less than pageSize total pages so show all
       startPage = 1;
       endPage = totalPages;
     } else {
-      // more than 10 total pages so calculate start and end pages
-      if (currentPage <= 6) {
+      // more than pageSize total pages so calculate start and end pages
+      if (currentPage <= ((pageSize / 2) + 1)) {
         startPage = 1;
-        endPage = 10;
-      } else if (currentPage + 4 >= totalPages) {
-        startPage = totalPages - 9;
+        endPage = pageSize;
+      } else if (currentPage + ((pageSize / 2) - 1) >= totalPages) {
+        startPage = totalPages - (pageSize - 1);
         endPage = totalPages;
       } else {
-        startPage = currentPage - 5;
-        endPage = currentPage + 4;
+        startPage = currentPage - (pageSize / 2);
+        endPage = currentPage + ((pageSize / 2) - 1);
       }
     }
 
@@ -29,11 +30,18 @@ export class PagerService {
     const startIndex = (currentPage - 1) * pageSize;
     const endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
 
+    console.log("this is startPage" + startPage)
+    console.log("this is endPage" + endPage)
+    console.log("this is startIndex" + startIndex)
+    console.log("this is endIndex" + endIndex)
     // create an array of pages to ng-repeat in the pager control
     const pages = Array.from(
       Array(endPage + 1 - startPage),
       (_, i) => startPage + i
     );
+    console.log("this is pages" + pages)
+    console.log("this is currentPage" + currentPage)
+    console.log("this is totalPages" + totalPages)
     // return object with all pager properties required by the view
     return {
       totalItems: totalItems,
