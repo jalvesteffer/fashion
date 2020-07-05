762006ca3d7856c40ad02f5cd00aebc6d2125e3e
jalvesteffer <jason.alvesteffer@smoothstack.com>
2020-07-05 13:26:02 -0400

    Alert works

--- src/app/register/returns/returns.component.ts
+++ src/app/register/returns/returns.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ShopService } from "../../common/services/shop.service";
 import { environment } from "../../../environments/environment";
 import { PagerService } from "../../common/services/pager.service";
@@ -11,10 +11,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./returns.component.css']
 })
 export class ReturnsComponent implements OnInit {
+  @ViewChild('alert', {}) alert: ElementRef;
 
   headerText: string = "In-Store Returns";
   headerImage: string = "instore-returns-start";
-  message: string;
+  message: string = "No Message";
 
   // transactions data
   transactions: any;
@@ -38,12 +39,21 @@ export class ReturnsComponent implements OnInit {
   ngOnInit() {
     this.loadAllOpenTransactions();
     this.initializeFormGroup();
+    this.closeAlert();
   }
 
   initializeFormGroup() {
     this.searchForm = new FormGroup({
       searchString: new FormControl(this.searchString),
     });
+  }
+
+  closeAlert() {
+    this.alert.nativeElement.classList.remove('show');
+  }
+
+  openAlert() {
+    this.alert.nativeElement.classList.add('show');
   }
 
   search() {
@@ -106,6 +116,7 @@ export class ReturnsComponent implements OnInit {
         this.loadAllOpenTransactions();
         this.setPage(1);
         this.message = "Order Number " + transaction.transactionId + " for " + transaction.user.username + " has been refunded " + "$" + transaction.total.toFixed(2);
+        this.openAlert();
       },
         (error) => {
           console.log("Error in refund()");
