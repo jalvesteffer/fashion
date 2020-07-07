49e1b9e5e372e2b838d0cd2158ca3a41de591adb
lexieAA <av697@nau.edu>
2020-07-06 18:59:37 -0700

    register look update

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -23,20 +23,162 @@ export class RegisterComponent implements OnInit {
   // inventory data
   inventory: any;
   sku: string;
+  sizeChoice: string;
+
+  // cart data
+  cartDetailsForm: FormGroup;
+  cartTotal: number = 0;
+  cartNumItems: number = 0;
+  cartProducts: any = [];
+  currentItem: any;
+  taxRate: number = 0.06;
+  couponCode: number;
+  couponApplied: any;
+  couponDiscount: number = 0;
+  couponDescription: string;
 
   constructor(
     private shopService: ShopService,
     private authService: AuthService,
+    private modalService: NgbModal,
+    private router: Router,
   ) { }
 
   ngOnInit() {
     this.initializeFormGroup();
+    this.loadCart(Number(localStorage.getItem('userId')));
   }
 
   initializeFormGroup() {
+    this.cartDetailsForm = new FormGroup({
+      cartNumItems: new FormControl(this.cartNumItems),
+      cartProducts: new FormControl(this.cartProducts),
+      sizeChoice: new FormControl([this.sizeChoice]),
+      couponCode: new FormControl([this.couponCode]),
+    });
+
     this.searchInventoryForm = new FormGroup({
       searchString: new FormControl(this.searchString),
     });
+  }
+
+  loadCart(userId: number) {
+    this.shopService.getAll(`${environment.shopUrl}${environment.getCartItemsURI}${userId}`)
+      .subscribe((res) => {
+        this.cartProducts = res;
+        this.cartNumItems = this.cartProducts.length;
+
+        this.cartTotal = 0;
+        let p = null;
+        for (p of this.cartProducts) {
+          this.cartTotal += p.price;
+        }
+      },
+        (error) => {
+          throw new Error("Error in loadCart()");
+        }
+      );
+  }
+
+  loadCoupon(userId: number) {
+    this.shopService.getAll(`${environment.shopUrl}${environment.getCouponURI}${userId}`)
+      .subscribe((res) => {
+        this.couponApplied = res;
+        if (this.couponApplied) {
+          this.couponCode = this.couponApplied.couponId;
+          this.couponDescription = this.couponApplied.couponDesc;
+          this.couponDiscount = this.couponApplied.discount;
+        }
+
+      },
+        (error) => {
+          console.log("No coupons applied");
+        }
+      );
+  }
+
+  addToCart(itemSku: number) {
+
+    // create a new transaction with item user added to cart
+    const transaction = {
+      storeId: 1,
+      paymentId: 123,
+      userId: Number(localStorage.getItem('userId')),
+      status: "open",
+      inventory: [{
+        sku: itemSku
+      }]
+    }
+
+    // call sales service to create the transaction if no open transaction already exists;
+    // otherwise, the existing open transaction will be updated
+    this.shopService.postObj(`${environment.shopUrl}${environment.postTransactionURI}`, transaction)
+      .subscribe((res) => {
+        this.modalService.dismissAll();
+
+        this.loadCart(Number(localStorage.getItem('userId')));
+      },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  removeItem(sku: number) {
+
+    this.shopService.deleteObj(`${environment.shopUrl}${environment.deleteTransactionURI}${Number(localStorage.getItem('userId'))}/sku/${sku}`)
+      .subscribe((res) => {
+        this.loadCart(Number(localStorage.getItem('userId')));
+      },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  addCouponCode(couponCode: number) {
+    // create a new transaction with user's coupon code
+    const transaction = {
+      userId: Number(localStorage.getItem('userId')),
+      coupons: [{
+        couponId: couponCode
+      }]
+    }
+
+    // call sales service to create the transaction if no open transaction already exists;
+    // otherwise, the existing open transaction will be updated
+    this.shopService.postObj(`${environment.shopUrl}${environment.postCouponURI}`, transaction)
+      .subscribe((res) => {
+
+        this.loadCoupon(Number(localStorage.getItem('userId')));
+      },
+        (error) => {
+          this.couponCode = 0;
+          this.couponDiscount = 0;
+          this.loadCoupon(Number(localStorage.getItem('userId')));
+        }
+      );
+  }
+
+  checkout(totalTax: number, totalBill: number) {
+    console.log("Checkout method, Tax: " + totalTax.toFixed(2) + ", Total: " + totalBill.toFixed(2));
+
+    const values = {
+      userId: Number(localStorage.getItem('userId')),
+      tax: totalTax,
+      total: totalBill
+    }
+
+    this.shopService.updateObj(`${environment.shopUrl}${environment.updateTransactionURI}`, values)
+      .subscribe((res) => {
+        this.modalService.dismissAll();
+        this.router.navigate(['gcfashions/shop/checkout']);
+        //.navigate(['checkout']);
+      },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 
   logout() {
@@ -58,9 +200,14 @@ export class RegisterComponent implements OnInit {
           (error) => {
             this.searchString = "";
             this.inventory = [];
+            this.sku = "No SKU Matching Found."
             console.log("mistake");
           }
         );
+    }else{
+      this.inventory = [];
+      this.sku = null;
+      this.searchString = null;
     }
   }
 
