d3ba4d52415ce681cc5e6572b61357ff3ecc3755
lexieAA <av697@nau.edu>
2020-07-29 21:18:31 -0700

    added sort pipes

--- src/app/common/subcategory-sort.pipe.ts
+++ src/app/common/subcategory-sort.pipe.ts
@@ -1,15 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'productSort'
+  name: 'subcategorySort'
 })
-export class ProductSortPipe implements PipeTransform {
+export class SubcategorySortPipe implements PipeTransform {
 
   transform(input: any[]): any {
     if (input) {
       return input.sort((a, b) => {
-        let an1: string = a.productName.toLowerCase();
-        let an2: string = b.productName.toLowerCase();
+        let an1: string = a.subcatName.toLowerCase();
+        let an2: string = b.subcatName.toLowerCase();
         return an1 < an2 ? -1 : an1 > an2 ? 1 : 0;
       });
     }
